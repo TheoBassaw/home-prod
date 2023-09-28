95c9dcbd257d4b37ed543a49f5918ebe6075e456
Theophilus Bassaw <theo@paradisenetworkz.com>
2023-09-28 16:11:56 -0400

    A lot of refactoring of ansible roles

--- terraform/control-plane/zerotier.tf
+++ terraform/control-plane/zerotier.tf
@@ -9,6 +9,11 @@ resource "zerotier_network" "router_overlay" {
     target = "10.30.0.0/24"
   }
 
+  route {
+    target = "10.30.1.0/24"
+    via    = "10.30.0.7"
+  }
+
   enable_broadcast = true
   private          = true
   flow_rules       = "accept;"
@@ -23,6 +28,11 @@ resource "zerotier_network" "personal_devices" {
 
   route {
     target = "10.30.1.0/24"
+  }
+
+  route {
+    target = "10.30.0.0/16"
+    via    = "10.30.1.1"
   }
 
   enable_broadcast = true
