1aaf28f64949e439fe07cc0f5b547d7390e38f82
Theophilus Bassaw <theo@paradisenetworkz.com>
2023-10-17 11:26:13 -0400

    Added logging, and improved hypervisor deployments. A lot of refactoring

--- control-plane/terraform/zerotier.tf
+++ control-plane/terraform/zerotier.tf
@@ -14,16 +14,6 @@ resource "zerotier_network" "router_overlay" {
     target = "10.30.0.0/24"
   }
 
-  route {
-    target = "10.30.1.0/24"
-    via    = "10.30.0.3"
-  }
-
-  route {
-    target = "10.30.2.0/24"
-    via    = "10.30.0.4"
-  }
-
   enable_broadcast = true
   private          = true
   flow_rules       = "accept;"
--- hypervisors/terraform/zerotier.tf
+++ hypervisors/terraform/zerotier.tf
@@ -21,3 +21,23 @@ resource "doppler_secret" "hypervisor_home_1" {
     "zt_network": data.zerotier_network.router_overlay.id
   })
 }
+
+resource "zerotier_identity" "hypervisor_anghelo_1" {}
+
+resource "zerotier_member" "hypervisor_anghelo_1" {
+  name           = "hypervisor-anghelo-1"
+  member_id      = zerotier_identity.hypervisor_anghelo_1.id
+  network_id     = data.zerotier_network.router_overlay.id
+  ip_assignments = ["10.30.0.11"]
+}
+
+resource "doppler_secret" "hypervisor_anghelo_1" {
+  project = "hypervisors"
+  config = "prd"
+  name = "HYPERVISOR_ANGHELO_1_ZT"
+  value = yamlencode({
+    "zt_public": zerotier_identity.hypervisor_anghelo_1.public_key
+    "zt_private": zerotier_identity.hypervisor_anghelo_1.private_key
+    "zt_network": data.zerotier_network.router_overlay.id
+  })
+}
