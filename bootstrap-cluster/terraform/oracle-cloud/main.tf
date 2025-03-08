b518208911b981c861e6d94a2d2776f3d6c81203
TheDarkBrotherhood <theo@paradisenetworkz.com>
2025-03-08 02:16:34 -0500

    Created Bootstrap Cluster

---
+++ bootstrap-cluster/terraform/oracle-cloud/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    oci = {
+      source  = "oracle/oci"
+      version = ">= 5.33.0"
+    }
+  }
+
+  backend "remote" {
+    hostname     = "app.terraform.io"
+	organization = "home-prod"
+	workspaces {
+	  name = "oracle-cloud"
+	}
+  }
+}
+
+data "oci_identity_availability_domains" "ads" {
+  compartment_id = var.compartment_id
+}
+
+data "oci_objectstorage_namespace" "namespace" {
+  compartment_id = var.compartment_id
+}
+
+data "oci_containerengine_cluster_kube_config" "kube_config" {
+  cluster_id = oci_containerengine_cluster.bootstrap-cluster.id
+  depends_on = [oci_containerengine_node_pool.node_pool]
+}
---
+++ bootstrap-cluster/terraform/oracle-cloud/vcn.tf
@@ -0,0 +1,98 @@
+resource "oci_core_vcn" "vcn" {
+  compartment_id          = var.compartment_id
+  display_name            = "vcn"
+  cidr_blocks             = ["10.0.0.0/23"]
+  is_ipv6enabled          = true
+}
+
+resource "oci_core_internet_gateway" "ig" {
+  compartment_id = var.compartment_id
+  vcn_id         = oci_core_vcn.vcn.id
+  display_name   = "internet-gateway"
+  enabled        = true
+}
+
+resource "oci_core_route_table" "ig_route_table" {
+  compartment_id = var.compartment_id
+  vcn_id         = oci_core_vcn.vcn.id
+  display_name   = "ig_route_table"
+
+  route_rules {
+    network_entity_id = oci_core_internet_gateway.ig.id
+    description       = "Default Route"
+    destination       = "0.0.0.0/0"
+    destination_type  = "CIDR_BLOCK"
+  }
+}
+
+resource "oci_core_security_list" "public_subnet_sl" {
+  compartment_id = var.compartment_id
+  vcn_id         = oci_core_vcn.vcn.id
+  display_name   = "public-subnet-sl"
+
+  egress_security_rules {
+    description      = "To Internet"
+    protocol         = "all"
+    destination      = "0.0.0.0/0"
+    destination_type = "CIDR_BLOCK"
+  }
+
+  ingress_security_rules {
+    description = "Allow VCN"
+    protocol    = "all"
+    source      = "10.0.0.0/23"
+    source_type = "CIDR_BLOCK"
+  }
+
+  ingress_security_rules {
+    description = "Allow VCN"
+    protocol    = 6
+    source      = "0.0.0.0/0"
+    source_type = "CIDR_BLOCK"
+
+    tcp_options {
+      max = 6443
+      min = 6443
+    }
+  }
+}
+
+resource "oci_core_security_list" "public_subnet_sl_dos" {
+  compartment_id = var.compartment_id
+  vcn_id         = oci_core_vcn.vcn.id
+  display_name   = "public-subnet-sl-dos"
+
+  egress_security_rules {
+    description      = "To Internet"
+    protocol         = "all"
+    destination      = "0.0.0.0/0"
+    destination_type = "CIDR_BLOCK"
+  }
+
+  ingress_security_rules {
+    description = "Allow VCN"
+    protocol    = "all"
+    source      = "10.0.0.0/23"
+    source_type = "CIDR_BLOCK"
+  }
+}
+
+resource "oci_core_subnet" "public_subnet" {
+  compartment_id    = var.compartment_id
+  cidr_block        = "10.0.0.0/24"
+  ipv6cidr_blocks   = [cidrsubnet(oci_core_vcn.vcn.ipv6cidr_blocks[0], 8, 0)]
+  vcn_id            = oci_core_vcn.vcn.id
+  display_name      = "public-subnet"
+  security_list_ids = [oci_core_security_list.public_subnet_sl.id]
+  route_table_id    = oci_core_route_table.ig_route_table.id
+}
+
+resource "oci_core_subnet" "public_subnet_dos" {
+  compartment_id    = var.compartment_id
+  cidr_block        = "10.0.1.0/24"
+  ipv6cidr_blocks   = [cidrsubnet(oci_core_vcn.vcn.ipv6cidr_blocks[0], 8, 1)]
+  vcn_id            = oci_core_vcn.vcn.id
+  display_name      = "public-subnet-dos"
+  security_list_ids = [oci_core_security_list.public_subnet_sl_dos.id]
+  route_table_id    = oci_core_route_table.ig_route_table.id
+}
