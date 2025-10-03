6ec5afe21877220b28da5d30eb18e44aaf7af626
TheDarkBrotherhood <bassawdavid@gmail.com>
2025-10-03 16:00:58 -0400

    More cleanup and refactoring

--- terraform/talos/cluster/main.tf
+++ terraform/talos/cluster/main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket                      = "terraform"
-    key                         = "cluster/talos.tfstate"
+    key                         = "talos/cluster.tfstate"
     region                      = "auto"
     skip_credentials_validation = true
     skip_metadata_api_check     = true
@@ -22,7 +22,8 @@ terraform {
 }
 
 locals {
-  cluster-name = "home-cluster"
+  cluster-name  = "home-cluster"
+  talos-version = "1.11.2"
   nodes = {
     talos-control-home-1 = {
       machine_type = "controlplane"
@@ -45,12 +46,25 @@ data "talos_machine_configuration" "machine_configuration" {
   machine_type     = each.value.machine_type
   cluster_endpoint = "https://10.20.0.200:6443"
   machine_secrets  = talos_machine_secrets.machine_secrets.machine_secrets
-  talos_version    = "1.11.2"
+  talos_version    = local.talos-version
+  config_patches   = [
+    file("${path.root}/all.yaml"),
+    file("${path.root}/${each.key}.yaml"),
+    yamlencode({
+      machine = {
+        install = {
+          image = "factory.talos.dev/metal-installer/${talos_image_factory_schematic.schematic.id}:v${local.talos-version}"
+        }
+      }
+    })
+  ]
 }
 
 data "talos_client_configuration" "client_configuration" {
   cluster_name         = local.cluster-name
   client_configuration = talos_machine_secrets.machine_secrets.client_configuration
+  nodes                = [for node in local.nodes: "${node.apply_ip}"]
+  endpoints            = [for node in local.nodes: "${node.apply_ip}"]
 }
 
 resource "talos_machine_secrets" "machine_secrets" {}
@@ -59,9 +73,31 @@ resource "talos_image_factory_schematic" "schematic" {
   schematic = file("${path.root}/schematic.yaml")
 }
 
-resource "talos_machine_configuration_apply" "this" {
+resource "talos_machine_configuration_apply" "configuration_apply" {
   for_each                    = local.nodes
-  client_configuration        = data.talos_client_configuration.client_configuration
-  machine_configuration_input = data.talos_machine_configuration.machine_configuration.machine_configuration
+  client_configuration        = talos_machine_secrets.machine_secrets.client_configuration
+  machine_configuration_input = data.talos_machine_configuration.machine_configuration[each.key].machine_configuration
   node                        = each.value.apply_ip
+}
+
+resource "talos_machine_bootstrap" "bootstrap" {
+  node                 = element([for node in local.nodes: "${node.apply_ip}"], 0)
+  client_configuration = talos_machine_secrets.machine_secrets.client_configuration
+  depends_on           = [talos_machine_configuration_apply.configuration_apply]
+}
+
+resource "talos_cluster_kubeconfig" "kubeconfig" {
+  client_configuration         = talos_machine_secrets.machine_secrets.client_configuration
+  node                         = element([for node in local.nodes: "${node.apply_ip}"], 0)
+  depends_on                   = [talos_machine_bootstrap.bootstrap]
+}
+
+resource "local_sensitive_file" "kubeconfig" {
+  content  = talos_cluster_kubeconfig.kubeconfig.kubeconfig_raw
+  filename = pathexpand("~/.kube/config")
+}
+
+resource "local_sensitive_file" "talosconfig" {
+  content  = data.talos_client_configuration.client_configuration.talos_config
+  filename = pathexpand("~/.talos/config")
 }
